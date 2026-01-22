0439828acfd280792ec493689e250b99318b06d6
Deepu Sahoo <89370639+07Deepu@users.noreply.github.com>
2026-01-22 16:11:19 +0530

    Add minimal PHPUnit test and CI pipeline

---
+++ tests/Feature/SmokeTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class SmokeTest extends TestCase
+{
+    /** @test */
+    public function application_can_run()
+    {
+        $this->assertTrue(true);
+    }
+}
